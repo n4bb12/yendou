f2e16976d1966291ef9631771123a037aa4b4dd7
Abraham Schilling <6810177+n4bb12@users.noreply.github.com>
2026-05-22 20:03:36 +0200

    refactor(App): remove unused toaster effect and imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
-import { useEffect } from "react"
 import { Counter } from "./components/counter/Counter"
-import { toaster } from "./components/ui/toaster"
 
 function App() {
-  useEffect(() => {
-    toaster.success({
-      title: "Incremented",
-      description: "Counter is now 3",
-      duration: Number.MAX_SAFE_INTEGER,
-    })
-  }, [])
+  // useEffect(() => {
+  //   toaster.success({
+  //     title: "Incremented",
+  //     description: "Counter is now 3",
+  //     duration: Number.MAX_SAFE_INTEGER,
+  //   })
+  // }, [])
 
   return (
     <div className="grid w-full grid-cols-1 gap-12 p-12">
